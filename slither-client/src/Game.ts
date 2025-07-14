7fde4c6dfb2e1a3cd36579579ac2fac0a5e6dab8
WhiteToggled <toggledwhite@gmail.com>
2025-07-14 18:48:02 +0500

    feat: score overlay

--- slither-client/src/Game.ts
+++ slither-client/src/Game.ts
@@ -2,12 +2,17 @@ import { Application, Point } from "pixi.js";
 import { World } from "./World";
 import { Player } from "./Player";
 import { Input } from "./Input";
+import { Overlay } from "./Overlay";
 
 export class Game {
     private app: Application;
     private input!: Input;
     private world!: World;
+
     private player!: Player;
+    private lastScore: number = 0;
+
+    private overlay!: Overlay;
     private screenCenter: Point = new Point();
 
     constructor(app: Application) {
@@ -27,28 +32,36 @@ export class Game {
         this.world = new World();
         this.app.stage.addChild(this.world.container);
 
+        this.overlay = new Overlay();
+        this.app.stage.addChild(this.overlay.container);
+
         this.player = new Player();
-        this.world.addEntity(this.player.sprite);
+        this.world.add(this.player.sprite);
 
         this.input = new Input(this.app.canvas);
 
+        // positioning
         this.updateScreenCenter();
         this.world.setPosition(this.screenCenter);
-
         window.addEventListener("resize", () => {
             this.updateScreenCenter();
             this.world.setPosition(this.screenCenter);
         });
 
-        this.app.ticker.add((delta) => this.render(delta.deltaTime));
+        this.app.ticker.add((delta) => this.update(delta.deltaTime));
     }
     
     private updateScreenCenter() {
         this.screenCenter.set(this.app.screen.width / 2, this.app.screen.height / 2);
     }
 
-    private render(delta: number) {
+    private update(delta: number) {
         this.player.update(this.input.mousePos, this.screenCenter);
-        this.world.update(this.player.sprite.position, delta);
+        this.world.update(this.player, delta, (score: number) => {
+            if (score !== this.lastScore) {
+                this.lastScore = score;
+                this.overlay.setScore(score);
+            }
+        });
     }
 }
---
+++ slither-client/src/Overlay.ts
@@ -0,0 +1,27 @@
+import { Container, Text } from "pixi.js";
+
+export class Overlay {
+    public container: Container;
+    private scoreText: Text;
+
+    constructor() {
+        this.container = new Container();
+        this.scoreText = new Text({
+            text: "Score: 0",
+            style: {
+                fontFamily: "monospace",
+                fontSize: 20,
+                fill: "white",
+            }
+        });
+        this.container.addChild(this.scoreText);
+    }
+
+    public setScore(score: number) {
+        this.scoreText.text = `Score: ${score}`;
+    }
+
+    public resize(screenWidth: number, screenHeight: number) {
+        this.scoreText.position.set(10, screenHeight - this.scoreText.height - 10);
+    }
+}
